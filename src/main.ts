38fd0dfb2b8db02b161274044c266420b42c3fb0
Oki <okipratama97@gmail.com>
2024-01-21 20:44:41 +0700

    feat: added cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common'
 import { report } from './middlewares/report.middleware'
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule)
+	const app = await NestFactory.create(AppModule, { cors: true })
 	app.setGlobalPrefix('api')
 	app.useGlobalPipes(
 		new ValidationPipe({
